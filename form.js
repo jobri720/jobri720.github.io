efd4aec6cb9247a7c22dd341bb5844cd3440160b
jobri720 <jobri720@users.noreply.github.com>
2019-01-15 16:02:21 -0500

    Update form.js

--- form.js
+++ form.js
@@ -1,10 +1,12 @@
-require('style.css')
-import fs from 'fs';
-const string = fs.readFileSync('.' + '/style.css', 'utf8');
+
 //jQuery time
 var current_fs, next_fs, previous_fs; //fieldsets
 var left, opacity, scale; //fieldset properties which we will animate
 var animating; //flag to prevent quick multi-click glitches
+
+require('style.css')
+import fs from 'fs';
+const string = fs.readFileSync('.' + '/style.css', 'utf8');
 
 $(".next").click(function(){
 	if(animating) return false;
